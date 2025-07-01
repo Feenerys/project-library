ace5fdbd944962709751dd2fa3af4a2c8be88cd0
Finn Nguon <36335479+Feenerys@users.noreply.github.com>
2025-07-01 18:17:54 +1000

    feat: add read and unread status button and page numbers

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const myLibrary = [];
 
-function Book(title, author, read) {
+function Book(title, author, pages, read) {
   if (!new.target) {
     throw Error("You must use the 'new' operator to call the constructor");
   }
@@ -8,45 +8,56 @@ function Book(title, author, read) {
   this.title = title;
   this.author = author;
   this.read = read;
+  this.pages = pages;
 }
 
-function addBookToLibrary(title, author, status) {
-  const book = new Book(title, author, status);
+function addBookToLibrary(title, author, pages, status) {
+  const book = new Book(title, author, pages, status);
   myLibrary.push(book);
 }
 
 function removeFromLibrary(id) {
-  const index = myLibrary.findIndex(book => book.id == id);
+  const index = myLibrary.findIndex((book) => book.id == id);
   myLibrary.splice(index, 1);
 }
 
-addBookToLibrary("The Silent Forest", "Elena Woods", true);
-addBookToLibrary("Quantum Dreams", "Max Planckman", true);
-addBookToLibrary("Midnight Skies", "Luna Nightshade", false);
-addBookToLibrary("Code & Chaos", "Ada Sterling", false);
-addBookToLibrary("Echoes of Tomorrow", "J.D. Veritas", true);
+addBookToLibrary("The Silent Forest", "Elena Woods", 123, true);
+addBookToLibrary("Quantum Dreams", "Max Planckman", 423, true);
+addBookToLibrary("Midnight Skies", "Luna Nightshade", 234, false);
+addBookToLibrary("Code & Chaos", "Ada Sterling", 61, false);
+addBookToLibrary("Echoes of Tomorrow", "J.D. Veritas", 131, true);
 
 const list = document.querySelector(".book-list");
 
 function renderBook(book) {
   const item = document.createElement("li");
-  item.textContent = `${book.title} by ${book.author}, ${book.read ? "read" : "not read"}`;
+  item.textContent = `${book.title} by ${book.author}, ${book.pages} pages`;
   item.dataset.id = book.id;
 
   const deleteButton = document.createElement("button");
   deleteButton.textContent = "x";
-  deleteButton.className = "delete";
 
   deleteButton.addEventListener("click", () => {
     const element = document.querySelector(`[data-id="${item.dataset.id}"]`);
     element.remove();
     removeFromLibrary(book.id);
-    console.log(myLibrary)
+    console.log(myLibrary);
   });
 
   const readButton = document.createElement("button");
-  readButton.textContent = "read"
+  readButton.textContent = book.read ? "unread" : "read";
 
+  readButton.addEventListener("click", () => {
+    if (book.read == true) {
+      book.read = false;
+      readButton.textContent = "read";
+    } else {
+      book.read = true;
+      readButton.textContent = "unread";
+    }
+  });
+
+  item.appendChild(readButton);
   item.appendChild(deleteButton);
   list.appendChild(item);
 }
